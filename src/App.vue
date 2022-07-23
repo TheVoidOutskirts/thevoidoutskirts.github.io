664440d832603352f2ad43abafe413de823f97f6
Stefano Vittorio Porta <stefa168@hotmail.it>
2022-07-23 18:54:16 +0200

    First part of calculator flow

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <script setup lang="ts">
 import {RouterLink, RouterView} from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
+import {Personaggi} from "@/assets/data";
+import type {Personaggio} from "@/assets/data";
+import {Bar} from "vue-chartjs";
+import {computed, reactive, ref} from "vue";
+
+const attackerIndex = ref(-1)
+const attacker = computed(() => attackerIndex.value >= 0 ? Personaggi[attackerIndex.value] : null)
+
+const weaponIndex = ref(-1)
+
 </script>
 
 <template>
-<!--  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
+  <!--  <header>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!"/>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!"/>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView/>-->
+  <div class="container">
+    <h1>Calcolatore</h1>
+    <!-- Selezione attaccante -->
+    <div class="row">
+      <div class="col-3">
+        <h2>Scegli un attaccante</h2>
+        <div v-for="(character, index) in Personaggi">
+          <input type="radio" :value="index" v-model="attackerIndex">
+          <label :for="index">{{ character.nome }}</label>
+        </div>
+      </div>
+      <div class="col" v-if="attacker != null">
+        <h2>Dettagli personaggio</h2>
+        <div><span class="h4">Nome:</span> <span class="ps-3">{{ attacker?.nomeCompleto }}</span></div>
+        <div><span class="h4">Armatura:</span> <span class="ps-3">{{ attacker?.armatura }}</span></div>
+        <div>
+          <span class="h4">Competenza Attacco:</span> <span class="ps-3">{{ attacker?.competenzaAttacco }}</span>
+        </div>
+        <div>
+          <span class="h4">Competenza Difesa:</span> <span class="ps-3">{{ attacker?.competenzaDifesa }}</span>
+        </div>
+        <div class="row">
+          <div class="col-1">Armi:</div>
+          <div v-if="attacker?.armi.length === 0">Il personaggio non ha armi</div>
+          <div v-for="(weapon, index) in attacker?.armi">
+            <input type="radio" :value="index" v-model="weaponIndex">
+            <label :for="index">{{weapon}}</label>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
 
-  <RouterView/>-->
-  <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
+    <!-- Grafico probabilità attacco -->
+    <div class="row">
+      <div class="col" v-if="attackerIndex">
+        <h2>Probabilità di colpire</h2>
+        <!-- Bar chart -->
+      </div>
+      <div class="col" v-if="!attackerIndex" style="text-align: center">
+        <p>Seleziona un personaggio!</p>
+      </div>
+    </div>
   </div>
 
 </template>
