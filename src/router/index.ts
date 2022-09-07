e1480fee5d5eb9385791bc5422c7d117d8477d88
Stefano Vittorio Porta <stefa168@hotmail.it>
2022-09-07 19:20:37 +0200

    Revert "fix: Trying to fix urls"
    This reverts commit d432d4e382bf08569c2c14c4f8acafb8353bc510.

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,50 +7,46 @@ const router = createRouter({
     routes: [
         {
             path: '/',
-            redirect: {name: 'home'}
-        },
-        {
-            path: '/enigma-calculator/',
             name: 'home',
             component: HomeView
         },
         {
-            path: '/enigma-calculator/calculator',
+            path: '/calculator',
             name: 'calculator',
             component: () => import('../views/CalculatorView.vue')
         },
         {
-            path: '/enigma-calculator/naval',
+            path: '/naval',
             name: 'navalCalculator',
             component: () => import('../views/NavalCalculatorView.vue')
         },
         {
-            path: '/enigma-calculator/units',
+            path: '/units',
             name: 'unitViewer',
             component: () => import('../views/UnitViewerView.vue')
         },
         {
-            path: '/enigma-calculator/history',
+            path: '/history',
             name: 'history',
             component: () => import('../views/HistoryView.vue')
         },
         {
-            path: '/enigma-calculator/places',
+            path: '/places',
             name: 'places',
             component: () => import('../views/PlacesView.vue')
         },
         {
-            path: '/enigma-calculator/factions',
+            path: '/factions',
             name: 'factions',
             component: () => import('../views/FactionsView.vue')
         },
         {
-            path: '/enigma-calculator/maps',
+            path: '/maps',
             name: 'maps',
             component: () => import('../views/MapsView.vue')
         },
         {
-            path: '/enigma-calculator/rules',
+            path: '/rules',
             name: 'rules',
             component: () => import('../views/RulesView.vue')
         }
