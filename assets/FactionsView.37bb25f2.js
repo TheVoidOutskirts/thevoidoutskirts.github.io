8153fd24e5e19dc7d9c0206689ba334f4bcabb52
CI <CI@example.com>
2023-01-04 01:22:08 +0000

    deploy

---
+++ assets/FactionsView.37bb25f2.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,c as a}from"./index.13309b32.js";const n={};function r(o,t){return c(),a("h1",null,"Fazioni")}var _=e(n,[["render",r]]);export{_ as default};
---
+++ assets/PlacesView.d2fe5f04.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,c as r}from"./index.13309b32.js";const a={};function n(o,t){return c(),r("h1",null,"Luoghi")}var _=e(a,[["render",n]]);export{_ as default};
